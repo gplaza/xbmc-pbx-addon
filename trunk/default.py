9e17b4e24fda77aa25ee46adf601bb824f37e883
hmronline <hmronline@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2010-04-24 15:34:32 +0000

    More cosmetic changes, fixes and new improvements on settings and skinning.

--- trunk/default.py
+++ trunk/default.py
@@ -16,7 +16,7 @@ __credits__ = "XBMC Team, py-Asterisk"
 __version__ = "0.0.1"
 
 xbmc.output(__scriptname__ + " Version: " + __version__  + "\n")
-BASE_RESOURCE_PATH = xbmc.translatePath(os.path.join(os.getcwd(), 'resources', 'lib'))
+BASE_RESOURCE_PATH = xbmc.translatePath(os.path.join(os.getcwd(),'resources','lib'))
 sys.path.append(BASE_RESOURCE_PATH)
 __language__ = xbmc.Language(os.getcwd()).getLocalizedString
 
@@ -30,7 +30,7 @@ import Asterisk.Manager, Asterisk.Util
 #############################################################################################################
 def log(msg):
 	try:
-		xbmc.output("[%s]: %s\n" % (__scriptname__, msg))
+		xbmc.output("[%s]: %s\n" % (__scriptname__,msg))
 	except:
 		pass
 
@@ -40,11 +40,28 @@ if not os.path.isdir(xbmc.translatePath(XBMC_HOME)):	# if fails to convert to Q:
 	XBMC_HOME = 'Q:'
 log("XBMC_HOME=%s" % XBMC_HOME)
 
+ACTION_MOVE_LEFT = 1    
+ACTION_MOVE_RIGHT = 2
+ACTION_MOVE_UP = 3
+ACTION_MOVE_DOWN = 4
+ACTION_PAGE_UP = 5
+ACTION_PAGE_DOWN = 6
+ACTION_SELECT_ITEM = 7
+ACTION_HIGHLIGHT_ITEM = 8
+ACTION_PARENT_DIR = 9
+ACTION_PREVIOUS_MENU = 10
+ACTION_SHOW_INFO = 11
+
+ACTION_PAUSE = 12
+ACTION_STOP = 13
+ACTION_NEXT_ITEM = 14
+ACTION_PREV_ITEM = 15
+
+ACTION_EXIT_SCRIPT = (9, 10)
+
 #############################################################################################################
 class get_incoming_call(object):
-        '''
-        Subscribe to Asterisk events, and get incoming calls.
-        '''
+
         def __init__(self):
 		log("__init__()")
                 global xbmc_player
@@ -53,39 +70,39 @@ class get_incoming_call(object):
                 self.ast_uniqid = 0
                 self.events = Asterisk.Util.EventCollection()
                 self.events.clear()
-                self.events.subscribe('Newchannel', self.Newchannel)
-                self.events.subscribe('NewCallerid', self.NewCallerid)
-                self.events.subscribe('Hangup', self.Hangup)
+                self.events.subscribe('Newchannel',self.Newchannel)
+                self.events.subscribe('NewCallerid',self.NewCallerid)
+                self.events.subscribe('Hangup',self.Hangup)
 
 	#####################################################################################################
-        def Newchannel(self, pbx, event):
+        def Newchannel(self,pbx,event):
 		#log("> NewChannel()")
-                if event.ChannelStateDesc == 'Ring' and self.ast_uniqid == 0:
+                if (event.ChannelStateDesc == 'Ring' and self.ast_uniqid == 0):
                         self.ast_uniqid = event.Uniqueid
 
 	#####################################################################################################
-        def NewCallerid(self, pbx, event):
+        def NewCallerid(self,pbx,event):
 		#log("> NewCallerid()")
-                if event.Uniqueid == self.ast_uniqid and event.CallerIDName != "" and event.CallerIDNum != "":
+                if (event.Uniqueid == self.ast_uniqid and event.CallerIDName != "" and event.CallerIDNum != ""):
                         self.newcall_actions(event)
 
 	#####################################################################################################
-        def Hangup(self, pbx, event):
+        def Hangup(self,pbx,event):
 		#log("> Hangup()")
-                if event.Uniqueid == self.ast_uniqid:
+                if (event.Uniqueid == self.ast_uniqid):
                         self.ast_uniqid = 0
                         self.hangup_actions(event)
 
 	#####################################################################################################
-        def hangup_actions(self, event):
+        def hangup_actions(self,event):
                 log("> hangup_actions()")
-                if xbmc_player.isPlaying() == 1:
+                if (xbmc_player.isPlaying() == 1):
 			# Resume media
-			if self.xbmc_player_paused:
+			if (self.xbmc_player_paused):
                         	xbmc_player.pause()
 
 	#####################################################################################################
-        def newcall_actions(self, event):
+        def newcall_actions(self,event):
 		log("> newcall_actions()")
 		self.xbmc_player_paused = False
                 str_callerid = str(event.CallerIDName + "<"+ event.CallerIDNum +">")
@@ -96,13 +113,13 @@ class get_incoming_call(object):
                 xbmc_oncall_notification_timeout = int(arr_timeout[int(settings.getSetting("xbmc_oncall_notification_timeout"))])
                 xbmc_oncall_pause_media = settings.getSetting("xbmc_oncall_pause_media")
                 del settings
-                if xbmc_player.isPlaying() == 1:
+                if (xbmc_player.isPlaying() == 1):
                         log(">> XBMC is playing content...")
                         log("Remaining time: " + str(xbmc_player.getTotalTime() - xbmc_player.getTime()))
-                        if xbmc_player.isPlayingAudio() == 1:
+                        if (xbmc_player.isPlayingAudio() == 1):
                                 info_tag = xbmc_player.getMusicInfoTag(object)
                                 log("Music title: " + info_tag.getTitle())
-                        if xbmc_player.isPlayingVideo() == 1:
+                        if (xbmc_player.isPlayingVideo() == 1):
                                 info_tag = xbmc_player.getVideoInfoTag(object)
                                 log("Video title: " + info_tag.getTitle() + ", Rating: " + str(info_tag.getRating()))
 			# Pause Media
@@ -112,36 +129,34 @@ class get_incoming_call(object):
 				self.xbmc_player_paused = True
 		# Show Incoming Call Notification Popup
 		if (xbmc_oncall_notification == "true"):
-			str_to_execute = "XBMC.Notification(" + __language__(30050) + ", " + str_callerid +", " + str(xbmc_oncall_notification_timeout * 1000) + ")"
+			str_to_execute = "XBMC.Notification(" + __language__(30050) + "," + str_callerid +"," + str(xbmc_oncall_notification_timeout * 1000) + ")"
 			log(str_to_execute)
                 	xbmc.executebuiltin(str_to_execute)
 
 
 #############################################################################################################
 class MainGUI(xbmcgui.WindowXML):
-	# action codes
-	ACTION_EXIT_SCRIPT = (9, 10)
 
-	def __init__(self, *args, **kwargs):
+	def __init__(self,*args,**kwargs):
 		log("__init__()")
-		xbmcgui.WindowXML.__init__(self, *args, **kwargs)
+		xbmcgui.WindowXML.__init__(self,*args,**kwargs)
 
 	#####################################################################################################
         def onInit(self):
 		dialog = xbmcgui.DialogProgress()
 		# Starting...
-		dialog.create(__scriptname__, __language__(30061))
+		dialog.create(__scriptname__,__language__(30061))
 		# Reading Config...
-		dialog.update(0, __language__(30062))
+		dialog.update(0,__language__(30062))
 		self.getConfig()
 		# Fetching Asterisk Info...
-		dialog.update(25, __language__(30063))
+		dialog.update(25,__language__(30063))
 		self.getInfo()
 		# Displaying Asterisk Info...
-		dialog.update(50, __language__(30064))
-		self.showInfo("cdr")
+		dialog.update(50,__language__(30064))
+		self.showInfo()
 		# Done...
-		dialog.update(100, __language__(30065))
+		dialog.update(100,__language__(30065))
 		dialog.close()
 		del dialog
 		log("Done.")
@@ -157,70 +172,59 @@ class MainGUI(xbmcgui.WindowXML):
 		self.asterisk_outbound_extension = settings.getSetting("asterisk_outbound_extension")
 		self.asterisk_outbound_context = settings.getSetting("asterisk_outbound_context")
 		self.asterisk_info_url = settings.getSetting("asterisk_info_url")
+		self.asterisk_vm_mailbox = settings.getSetting("asterisk_vm_mailbox")
+		audio_format = ["wav","gsm","mp3"]
+		self.asterisk_vm_format = audio_format[int(settings.getSetting("asterisk_vm_format"))]
 		del settings
 
 	#####################################################################################################
 	def getInfo(self):
 		log("> getInfo()")
 		#log(self.asterisk_info_url)
-		f = urllib.urlopen(self.asterisk_info_url + "?vm&cdr")
+		f = urllib.urlopen(self.asterisk_info_url + "?vm&cdr&mailbox=" + self.asterisk_vm_mailbox)
 		self.dom = xml.dom.minidom.parse(f)
 		#log(self.dom.toxml())
 		f.close()
 		del f
 
 	#####################################################################################################
-	def showInfo(self, option):
-		log("> showInfo(" + option + ")")
+	def showInfo(self):
+		log("> showInfo()")
+		options = ["cdr","vm"]
+		i = 120
 		try:
 			xbmcgui.lock()
-			if (option == "cdr"):
-				self.getControl(110).setSelected(True)
-				self.getControl(111).setSelected(False)
-				self.getControl(120).reset()
-				self.getControl(120).setVisible(True)
-				self.getControl(120).setEnabled(True)
-				self.getControl(121).setVisible(False)
-				self.getControl(121).setVisible(False)
-			else:
-				self.getControl(110).setSelected(False)
-				self.getControl(111).setSelected(True)
-				self.getControl(121).reset()
-				self.getControl(121).setVisible(True)
-				self.getControl(121).setEnabled(True)
-				self.getControl(120).setVisible(False)
-				self.getControl(120).setVisible(False)
-			# Parse CDR/VM XML content
-			for node in self.dom.getElementsByTagName(option):
-				listitem = xbmcgui.ListItem()
-				for childNode in node.childNodes:
-					if (childNode.nodeName != "#text"):
-						if (childNode.firstChild):
-							listitem.setProperty(childNode.nodeName, childNode.firstChild.data)
-						else:
-							listitem.setProperty(childNode.nodeName, "")
-				if (option == "cdr"):
-					self.getControl(120).addItem(listitem)
-				else:
-					self.getControl(121).addItem(listitem)
-				del listitem
+			for option in options:
+				self.getControl(i).reset()
+				# Parse CDR/VM XML content
+				for node in self.dom.getElementsByTagName(option):
+					listitem = xbmcgui.ListItem()
+					for childNode in node.childNodes:
+						if (childNode.nodeName != "#text"):
+							if (childNode.firstChild):
+								listitem.setProperty(childNode.nodeName,childNode.firstChild.data)
+							else:
+								listitem.setProperty(childNode.nodeName,"")
+					self.getControl(i).addItem(listitem)
+					del listitem
+				i = i + 1
 			xbmcgui.unlock()
 		except:
-			log("ERROR: %s::%s (%d) - %s" % (self.__class__.__name__, sys.exc_info()[2].tb_frame.f_code.co_name, sys.exc_info()[2].tb_lineno, sys.exc_info()[1],))
+			log("ERROR: %s::%s (%d) - %s" % (self.__class__.__name__,sys.exc_info()[2].tb_frame.f_code.co_name,sys.exc_info()[2].tb_lineno,sys.exc_info()[1],))
 
 	#####################################################################################################
-	def onAction(self, action):
-		if (action in self.ACTION_EXIT_SCRIPT):
+	def onAction(self,action):
+		if (action in ACTION_EXIT_SCRIPT):
 			self.close()
 
-	def onClick(self, controlId):
+	def onClick(self,controlId):
 		#log("> onClick(" + str(controlId) + ")")
 		# Initiate outgoing call
 		if (controlId == 120):
 			number_to_call = self.getControl(120).getSelectedItem().getProperty("src")
 			if (number_to_call != ""):
 				dialog = xbmcgui.Dialog()
-				if (dialog.yesno(__scriptname__, __language__(30104) + " '" + number_to_call + "'?")):
+				if (dialog.yesno(__scriptname__,__language__(30104) + " '" + number_to_call + "'?")):
 					self.make_outgoing_call(number_to_call)
 				del dialog
 		# Play Voice Mail
@@ -228,15 +232,10 @@ class MainGUI(xbmcgui.WindowXML):
 			recindex = self.getControl(121).getSelectedItem().getProperty("recindex")
 			if (recindex != ""):
 				dialog = xbmcgui.Dialog()
-				if (dialog.yesno(__scriptname__, __language__(30105))):
-					xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(self.asterisk_info_url + "?recindex=" + recindex)
+				if (dialog.yesno(__scriptname__,__language__(30105))):
+					url_vm = self.asterisk_info_url +"?recindex="+ recindex +"&mailbox="+ self.asterisk_vm_mailbox +"&format="+ self.asterisk_vm_format
+					xbmc.Player(xbmc.PLAYER_CORE_MPLAYER).play(url_vm)
 				del dialog
-		# CDR
-		elif (controlId == 110):
-			self.showInfo("cdr")
-		# VM
-		elif (controlId == 111):
-			self.showInfo("vm")
 		# Settings
 		elif (controlId == 115):
 			settings = xbmc.Settings(path=os.getcwd())
@@ -244,13 +243,13 @@ class MainGUI(xbmcgui.WindowXML):
 			del settings
 			self.onInit()
 
-	def onFocus(self, controlId):
+	def onFocus(self,controlId):
 		pass
 
 	#####################################################################################################
-	def make_outgoing_call(self, number_to_call):
+	def make_outgoing_call(self,number_to_call):
 		log("> make_outgoing_call()")
-        	pbx = Manager((self.asterisk_manager_host, self.asterisk_manager_port), self.asterisk_manager_user, self.asterisk_manager_pass)
+        	pbx = Manager((self.asterisk_manager_host,self.asterisk_manager_port),self.asterisk_manager_user,self.asterisk_manager_pass)
         	pbx.Originate(self.asterisk_outbound_extension,self.asterisk_outbound_context,number_to_call,1)
 		del pbx
 		log("done.")
@@ -261,7 +260,7 @@ class FirstTimeGUI(xbmcgui.Window):
 
 	def __init__(self):
 		log("__init__()")
-		dialog = xbmcgui.ControlTextBox(1, 1, 600, 600, "font12", "0xFFFFFFFF")
+		dialog = xbmcgui.ControlTextBox(1,1,600,600,"font12","0xFFFFFFFF")
 		msg = ""
 		for i in range(1,10):
 			msg = msg + __language__(30000 + i) + "\n"
@@ -269,7 +268,7 @@ class FirstTimeGUI(xbmcgui.Window):
 		dialog.setText(msg)
 
 	#####################################################################################################
-	def onAction(self, action):
+	def onAction(self,action):
 		settings = xbmc.Settings(path=os.getcwd())
 		settings.openSettings()
 		del settings
@@ -292,21 +291,22 @@ if (run_mode != RUNMODE_SILENT):
 		log("Launching GUI...")
                 settings = xbmc.Settings(path=os.getcwd())
                 first_time_use = settings.getSetting("first_time_use")
-		settings.setSetting("first_time_use", "false")
+		settings.setSetting("first_time_use","false")
                 del settings
 		if (first_time_use == "true"):
 			ui = FirstTimeGUI()
 		else:
-			ui = MainGUI("script_xbmc-pbx-addon_main.xml", os.getcwd(), "Default")
+			ui = MainGUI("script_xbmc-pbx-addon_main.xml",os.getcwd(),"Default")
 		ui.doModal()
 	except:
-		str_to_execute = "XBMC.Notification(" +  __language__(30051) + ", " + str(sys.exc_info()[1]) + ", " + str(15 * 1000) + ")"
+		str_to_execute = "XBMC.Notification(" + __language__(30051) + "," + str(sys.exc_info()[1]) + "," + str(15 * 1000) + ")"
 		log(str_to_execute)
 		xbmc.executebuiltin(str_to_execute)
 	try:
 		del ui
 		self.dom.unlink()
 		del self.dom
+		sys.modules.clear()
 	except:
 		pass
 else:
@@ -318,17 +318,18 @@ else:
                 asterisk_manager_user = settings.getSetting("asterisk_manager_user")
                 asterisk_manager_pass = settings.getSetting("asterisk_manager_pass")
                 del settings
-		pbx = Manager((asterisk_manager_host, asterisk_manager_port), asterisk_manager_user, asterisk_manager_pass)
+		pbx = Manager((asterisk_manager_host,asterisk_manager_port),asterisk_manager_user,asterisk_manager_pass)
 		grab = get_incoming_call()
 		pbx.events += grab.events
 		pbx.serve_forever()
 	except:
-		str_to_execute = "XBMC.Notification(" +  __language__(30051) + ", " + str(sys.exc_info()[1]) + ", " + str(15 * 1000) + ")"
+		str_to_execute = "XBMC.Notification(" + __language__(30051) + "," + str(sys.exc_info()[1]) + "," + str(15 * 1000) + ")"
 		log(str_to_execute)
 		xbmc.executebuiltin(str_to_execute)
 	try:
 		del grab
 		del pbx
+		sys.modules.clear()
 	except:
 		pass
 
--- trunk/server_side/xbmc-pbx-addon.php
+++ trunk/server_side/xbmc-pbx-addon.php
@@ -5,123 +5,121 @@
 
 */
 
+// Script constants
+$__scriptname__ = "XBMC PBX Addon";
+$__author__ = "hmronline";
+$__url__ = "http://code.google.com/p/xbmc-pbx-addon/";
+$__svn_url__ = "http://xbmc-pbx-addon.googlecode.com/svn/trunk/xbmc-pbx-addon";
+$__credits__ = "XBMC Team, py-Asterisk";
+$__version__ = "0.0.1";
+
+// YOU MAY WANT TO CUSTOMIZE THIS:
 $cdr_filename = "/var/log/asterisk/cdr-csv/Master.csv";
-$vm_path  = '/var/spool/asterisk/voicemail/default/000/INBOX/';
-$audioformat = "wav";
+$vm_path  = '/var/spool/asterisk/voicemail/default/';
 
-if (isset($_GET['recindex'])) {
+//#############################################################################################################
+if (isset($_GET['recindex']) && isset($_GET['mailbox']) && isset($_GET['format'])) {
+	// VoiceMail Audio Download
 	// Based on http://www.freepbx.org/trac/browser/freepbx/branches/2.7/amp_conf/htdocs/recordings/misc/audio.php
-
- $path = $vm_path . "msg" . $_GET['recindex'] . "." . $audioformat;
-  
-  // See if the file exists
-  if (!is_file($path)) { die("<b>404 File not found!</b>"); }
-
-  // Gather relevent info about file
-  $size = filesize($path);
-  $name = basename($path);
-  $extension = strtolower(substr(strrchr($name,"."),1));
-
-  // This will set the Content-Type to the appropriate setting for the file
-  $ctype ='';
-  switch( $extension ) {
-    case "mp3": $ctype="audio/mpeg"; break;
-    case "wav": $ctype="audio/x-wav"; break;
-    case "gsm": $ctype="audio/x-gsm"; break;
-
-    // not downloadable
-    default: die("<b>404 File not found!</b>"); break ;
-  }
-
-  // need to check if file is mislabeled or a liar.
-  $fp=fopen($path, "rb");
-  if ($size && $ctype && $fp) {
-    header("Pragma: public");
-    header("Expires: 0");
-    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-    header("Cache-Control: public");
-    header("Content-Description: wav file");
-    header("Content-Type: " . $ctype);
-    header("Content-Disposition: attachment; filename=" . $name);
-    header("Content-Transfer-Encoding: binary");
-    header("Content-length: " . $size);
-    fpassthru($fp);
-  } 
+	$vm_path = $vm_path . $_GET['mailbox'] . "/INBOX/";
+	$path = $vm_path . "msg" . $_GET['recindex'] . "." . $_GET['format'];
+	// See if the file exists
+	if (!is_file($path)) { die("<b>404 File not found!</b>"); }
+	// Gather relevent info about file
+	$size = filesize($path);
+	$name = basename($path);
+	$extension = strtolower(substr(strrchr($name,"."),1));
+	// This will set the Content-Type to the appropriate setting for the file
+	$ctype ='';
+	switch($extension) {
+		case "mp3": $ctype="audio/mpeg"; break;
+		case "wav": $ctype="audio/x-wav"; break;
+		case "gsm": $ctype="audio/x-gsm"; break;
+		// not downloadable
+		default: die("<b>404 File not found!</b>"); break ;
+	}
+	// need to check if file is mislabeled or a liar.
+	$fp=fopen($path, "rb");
+	if ($size && $ctype && $fp) {
+		header("Pragma: public");
+		header("Expires: 0");
+		header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+		header("Cache-Control: public");
+		header("Content-Description: wav file");
+		header("Content-Type: " . $ctype);
+		header("Content-Disposition: attachment; filename=" . $name);
+		header("Content-Transfer-Encoding: binary");
+		header("Content-length: " . $size);
+		fpassthru($fp);
+	} 
 }
 else {
-
-$xmldoc = new DOMDocument();
-$xmldoc->formatOutput = true;
-$xmlroot = $xmldoc->createElement("pbx");
-$xmldoc->appendChild($xmlroot);
-
-if (isset($_GET["cdr"])) {
-// CDR
-$cdr_fields = array('accountcode','src','dst','dcontext',
-	'clid','channel','dstchannel','lastapp','lastdata',
-	'start','answer','end','duration','billsec',
-	'disposition','amaflags','uniqueid','userfield');
-
-// Read CSV file and store into an Array
-$cdr = array();
-if (is_readable($cdr_filename)) {
-	if (($handle = fopen($cdr_filename, "r")) !== FALSE) {
-		while (($cdr_data = fgetcsv($handle, 4096, ",")) !== FALSE) {
-			$cdr[] = $cdr_data;
+	$xmldoc = new DOMDocument();
+	$xmldoc->formatOutput = true;
+	$xmlroot = $xmldoc->createElement("pbx");
+	$xmldoc->appendChild($xmlroot);
+	if (isset($_GET["cdr"])) {
+		// CDR
+		$cdr_fields = array('accountcode','src','dst','dcontext',
+		'clid','channel','dstchannel','lastapp','lastdata',
+		'start','answer','end','duration','billsec',
+		'disposition','amaflags','uniqueid','userfield');
+		// Read CSV file and store into an Array
+		$cdr = array();
+		if (is_readable($cdr_filename)) {
+			if (($handle = fopen($cdr_filename, "r")) !== FALSE) {
+				while (($cdr_data = fgetcsv($handle, 4096, ",")) !== FALSE) {
+					$cdr[] = $cdr_data;
+				}
+			fclose($handle);
+			}
 		}
-		fclose($handle);
+		// Filter, resize and reverse
+		$cdr = array_slice($cdr,-50);
+		$cdr = array_reverse($cdr);
+		// Convert CDR Array into XML
+		for ($i=0; $i < count($cdr); $i++) {
+			$node = $xmldoc->createElement("cdr");
+			for ($c=0; $c < count($cdr[$i]); $c++) {
+				$element = $xmldoc->createElement($cdr_fields[$c]);
+				$element->appendChild($xmldoc->createTextNode($cdr[$i][$c]));
+				$node->appendChild($element);
+			}
+			$xmlroot->appendChild($node);
+		}
+		unset($cdr);
 	}
-}
-
-// Filter, resize and reverse
-$cdr = array_slice($cdr,-50);
-$cdr = array_reverse($cdr);
-
-// Convert CDR Array into XML
-for ($i=0; $i < count($cdr); $i++) {
-	$node = $xmldoc->createElement("cdr");
-	for ($c=0; $c < count($cdr[$i]); $c++) {
-		$element = $xmldoc->createElement($cdr_fields[$c]);
-		$element->appendChild($xmldoc->createTextNode($cdr[$i][$c]));
-		$node->appendChild($element);
+	if (isset($_GET["vm"]) && isset($_GET['mailbox'])) {
+		// VoiceMail
+		$vm_path = $vm_path  . $_GET['mailbox'] . "/INBOX/";
+		if ($handle = opendir($vm_path)) {
+			$vm = array();
+			while (false !== ($file = readdir($handle))) {
+				if ($file != "." && $file != ".." && strpos($file,".txt")) {
+					$vm[str_replace(".txt","",str_replace("msg","",$file))] = parse_ini_file($vm_path . $file);
+				}
+			}
+		closedir($handle);
+		}
+		// Convert VM Array into XML
+		foreach ($vm as $i => $c) {
+			$node = $xmldoc->createElement("vm");
+			$element = $xmldoc->createElement(recindex);
+			$element->appendChild($xmldoc->createTextNode($i));
+			$node->appendChild($element);
+			foreach ($c as $key => $val) {
+				$element = $xmldoc->createElement($key);
+				$element->appendChild($xmldoc->createTextNode($val));
+				$node->appendChild($element);
+			}
+			$xmlroot->appendChild($node);
+		}
+		unset($vm);
 	}
-	$xmlroot->appendChild($node);
-}
-unset($cdr);
-}
-
-if (isset($_GET["vm"])) {
-// VoiceMail
-if ($handle = opendir($vm_path)) {
-    $vm = array();
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != ".." && strpos($file,".txt")) {
-            $vm[str_replace(".txt","",str_replace("msg","",$file))] = parse_ini_file($vm_path . $file);
-        }
-    }
-    closedir($handle);
-}
-
-// Convert VM Array into XML
-foreach ($vm as $i => $c) {
-	$node = $xmldoc->createElement("vm");
-	$element = $xmldoc->createElement(recindex);
-	$element->appendChild($xmldoc->createTextNode($i));
-	$node->appendChild($element);
-        foreach ($c as $key => $val) {
-                $element = $xmldoc->createElement($key);
-                $element->appendChild($xmldoc->createTextNode($val));
-                $node->appendChild($element);
-        }
-        $xmlroot->appendChild($node);
-}
-unset($vm);
-}
-
-// Print XML
-header ("content-type: text/xml");
-echo $xmldoc->saveXML();
-unset($xmldoc);
+	// Print XML
+	header ("content-type: text/xml");
+	echo $xmldoc->saveXML();
+	unset($xmldoc);
 }
 ?>
 
