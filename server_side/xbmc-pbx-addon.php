923125c98d727909bef7da20ed6dfb9dd9c6ba73
hmronline <hmronline@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2010-06-15 23:07:39 +0000

    Added instructions to edit path in autoexec.py as suggested by keylogthis on XBMC forums.

--- server_side/xbmc-pbx-addon.php
+++ server_side/xbmc-pbx-addon.php
@@ -13,8 +13,8 @@ $__url__                 = "http://code.google.com/p/xbmc-pbx-addon/";
 $__svn_url__             = "http://xbmc-pbx-addon.googlecode.com/svn/trunk/xbmc-pbx-addon";
 $__credits__             = "Team XBMC, py-Asterisk";
 $__started__             = "04-03-2010";
-$__date__                = "13-06-2010";
-$__version__             = "0.0.5";
+$__date__                = "15-06-2010";
+$__version__             = "0.0.6";
 
 // YOU MAY WANT TO CUSTOMIZE THIS:
 $cdr_filename = "/var/log/asterisk/cdr-csv/Master.csv";
