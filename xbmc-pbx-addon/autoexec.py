c66d41c9449954bcb3df33b033ac8966bfd5bc82
hmronline <hmronline@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2011-08-01 02:09:32 +0000

    Deleted all version.

--- xbmc-pbx-addon/autoexec.py
+++
@@ -1,8 +0,0 @@
-# Put this file into Q:\scripts folder to be executed when XBMC starts.
-import os
-import xbmc
-
-# Change this path according your XBMC setup (you may not need 'My Scripts')
-script_path = xbmc.translatePath(os.path.join(os.getcwd(),'My Scripts','xbmc-pbx-addon','bgservice.py'))
-xbmc.executescript(script_path)
-
