36d08616d152ae8c7cde4a1fb5bba90b51bdd241
oneadvent@gmail.com <oneadvent@gmail.com@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2011-08-05 23:14:33 +0000

    Removed 107

--- script.xbmc-pbx-addon/default.py
+++ script.xbmc-pbx-addon/default.py
@@ -104,7 +104,6 @@ class MainGUI(xbmcgui.WindowXML):
         del pbx
         log(">> " + asterisk_version)
         str_url = settings.getSetting("asterisk_info_url")
-        str_url = str_url +"?cdr"
         str_url = str_url +"?vm&cdr&mailbox="+ settings.getSetting("asterisk_vm_mailbox")
         str_url = str_url +"&vmcontext="+ settings.getSetting("asterisk_vm_context")
         del settings
