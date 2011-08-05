1aa8ad17540ec3f071544af122cb486fab5d78ef
oneadvent@gmail.com <oneadvent@gmail.com@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2011-08-05 21:07:01 +0000

    Took out VM pull so that it would work on some systems.

--- script.xbmc-pbx-addon/default.py
+++ script.xbmc-pbx-addon/default.py
@@ -104,8 +104,10 @@ class MainGUI(xbmcgui.WindowXML):
         del pbx
         log(">> " + asterisk_version)
         str_url = settings.getSetting("asterisk_info_url")
-        str_url = str_url +"?vm&cdr&mailbox="+ settings.getSetting("asterisk_vm_mailbox")
-        str_url = str_url +"&vmcontext="+ settings.getSetting("asterisk_vm_context")
+        str_url = str_url +"?cdr"
+        #took these out for the time being, VM is still in the process.
+        #str_url = str_url +"?vm&cdr&mailbox="+ settings.getSetting("asterisk_vm_mailbox")
+        #str_url = str_url +"&vmcontext="+ settings.getSetting("asterisk_vm_context")
         del settings
         if (self.DEBUG):
             log(">> " + str_url)
