b3ee95942fbf6aa2d874cc7deba60fe23b407783
hmronline <hmronline@dbfe1a56-27bb-11df-b6a2-4f9457dbae47>
2011-08-07 01:52:36 +0000

    bug fix and updated autoexec.py for Windows

--- trunk/script.xbmc-pbx-addon/autoexec.py
+++ trunk/script.xbmc-pbx-addon/autoexec.py
@@ -1,7 +1,8 @@
-# Put this file into the scripts (addons) folder to be executed when XBMC starts.
-# That is Q:\scripts on XBMC4Xbox.
-# Or /home/<username>/.xbmc/scripts on Linux XBMC pre-Dharma versions.
-# Or /home/<username>/.xbmc/userdata on Linux XBMC Dharma versions.
+# Put this file into the scripts (addons) folder to be executed when XBMC starts, which is:
+# /home/<username>/.xbmc/userdata on XBMC for Linux Dharma version;
+# /home/<username>/.xbmc/scripts on XBMC for Linux pre-Dharma version;
+# %APPDATA%\XBMC\userdata on XBMC for Windows Dharma version;
+# Q:\scripts on XBMC4XBOX 3.0.1 version;
 # If you already have an autoexec.py file there, just append this file contents to the existing one.
 
 import os
@@ -12,3 +13,4 @@ script_path = xbmc.translatePath(os.path.join(os.getcwd(),'..','addons','script.
 xbmc.executescript(script_path)
 
 # This file should be deprecated for XBMC Eden release.
+
